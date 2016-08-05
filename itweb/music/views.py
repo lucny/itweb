885c4d06817dfec97d706f6f5e51aebf67a1ccc7
ML <lucny@sspu-opava.cz>
2016-08-05 17:21:15 +0200

    Připojení databáze, views s výpisy alb

--- itweb/music/views.py
+++ itweb/music/views.py
@@ -1,4 +1,22 @@
 from django.http import HttpResponse
+from .models import Album
 
 def index(request):
-    return HttpResponse('<h1>Pokus</h1>')
+    html = ''
+    all_albums = Album.objects.all()
+    html += '<h1>Hudba - seznam alb</h1>'
+    html += '<ul>'
+    for album in all_albums:
+        url = '/music/' + str(album.id) + '/'
+        html += '<li><a href="'+url+'">'+ album.album_title +'</a></li>'
+    html += '</ul>'
+    return HttpResponse(html)
+
+def detail(request, album_id):
+    html = ''
+    album = Album.objects.get(id=album_id)
+    html += '<h1>Hudba - detail o albu</h1>'
+    html += '<h2>' + album.album_title + '</h2>'
+    html += '<p>Umelec: ' + album.artist + '</p>'
+    html += '<p>Zanr: ' + album.genre + '</p>'
+    return HttpResponse(html)
