eeba4febec18d5621e50d6c0edce7391f65b93fa
ML <lucny@sspu-opava.cz>
2016-08-05 22:29:53 +0200

    Použití formuláře

--- itweb/music/urls.py
+++ itweb/music/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     # /music/id/
     url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
+    # /music/id/favorite
+    url(r'^(?P<album_id>[0-9]+)/favorite/$', views.favorite, name='favorite'),
+
 ]
