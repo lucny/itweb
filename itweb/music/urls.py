b288e6ab232faf3f5c7dc3ee189e82b6776a4e8a
ML <lucny@sspu-opava.cz>
2016-08-09 23:49:54 +0200

    --all

--- itweb/music/urls.py
+++ itweb/music/urls.py
@@ -3,10 +3,11 @@ from . import views
 
 urlpatterns = [
     # /music/
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.IndexView.as_view(), name='index'),
     # /music/id/
-    url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
-    # /music/id/favorite
-    url(r'^(?P<album_id>[0-9]+)/favorite/$', views.favorite, name='favorite'),
-
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
+    # /music/album/add
+    url(r'album/add/$', views.AlbumCreate.as_view(), name='album-add'),
+    url(r'^album/(?P<pk>[0-9]+)$', views.AlbumUpdate.as_view(), name='album-update'),
+    url(r'^album/(?P<pk>[0-9]+)/delete/$', views.AlbumDelete.as_view(), name='album-delete'),
 ]
--- itweb/music/views.py
+++ itweb/music/views.py
@@ -1,24 +1,32 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Album, Song
+from django.views import generic
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.core.urlresolvers import reverse_lazy
+from .models import Album
 
-def index(request):
-    all_albums = Album.objects.all()
-    return render(request,'music/index.html',{'all_albums' : all_albums})
+class IndexView(generic.ListView):
+    template_name = 'music/index.html'
+    context_object_name = 'object_list'
 
-def detail(request, album_id):
-    album = get_object_or_404(Album, pk=album_id)
-    return render(request,'music/detail.html',{'album' : album})
+    def get_queryset(self):
+        return Album.objects.all()
 
-def favorite(request, album_id):
-    album = get_object_or_404(Album, pk=album_id)
-    try:
-        selected_song = album.song_set.get(pk=request.POST['song'])
-    except (KeyError, Song.DoesNotExist):
-        return render(request,'music/detail.html',{
-            'album' : album,
-            'error_message' : 'Nevybral jsi platnou pisen',
-        })
-    else:
-        selected_song.is_favorite = True
-        selected_song.save()
-        return render(request, 'music/detail.html', {'album': album})
+
+class DetailView(generic.DetailView):
+    model = Album
+    #context_object_name = 'album'
+    template_name = 'music/detail.html'
+
+
+class AlbumCreate(CreateView):
+    model = Album
+    fields = ['artist','album_title','genre','album_logo']
+
+
+class AlbumUpdate(UpdateView):
+    model = Album
+    fields = ['artist','album_title','genre','album_logo']
+
+
+class AlbumDelete(DeleteView):
+    model = Album
+    success_url = reverse_lazy('music:index')
